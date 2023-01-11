e59c4b66f7b789b5e0c9f3f9ce972a409c71a38a
William BernsteinCuevas <wertangio@gmail.com>
2023-01-10 21:36:21 -0500

    na

---
+++ Assets/Scripts/BaseClasses/ATKScript/ATKScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtkScript : MonoBehaviour
+{
+    /*
+        Script from which all battle moves' attack script field should be placed. These scripts are responsible for instantiating the
+        hitboxes required for the move as well as assign animations to the caster, among other features. 
+    */
+
+    public BattleMove parentMove;
+    public CharacterGameEntity targetEnemy;
+    public BattleManager battleManager;
+
+    public virtual void BeginMove(){}
+    public virtual void OnSuccess(){}
+    public virtual void OnFailure(){}
+}
